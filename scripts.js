c997daa875095a1466d6f7b075c4265f0b6932e7
Daniela Mical <daniela.m.torrez14@gmail.com>
2023-04-30 18:03:32 -0400

    Primeiro commit

--- scripts.js
+++ scripts.js
@@ -1,23 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const nameElement = document.querySelector('#name');
-    const usernameElement = document.querySelector('#username');
-    const avatarElement = document.querySelector('#avatar');
-    const reposElement = document.querySelector('#repos');
-    const followersElement = document.querySelector('#followers');
-    const followingElement = document.querySelector('#following');
-    const linkElement = document.querySelector('#link');
+$(document).ready(function() {
+    var url = 'https://api.github.com/users/wendeldh7';
 
-    fetch('https://api.github.com/users/ogiansouza')
-        .then(function(res) {
-            return res.json();
-        })
-        .then(function(json) {
-            nameElement.innerText = json.name;
-            usernameElement.innerText = json.login;
-            avatarElement.src = json.avatar_url;
-            followingElement.innerText = json.following;
-            followersElement.innerText = json.followers;
-            repos.innerText = json.public_repos;
-            linkElement.href = json.html_url;
-        })
-})
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        success: function(data) {
+        $('.profile-avatar').attr('src', data.avatar_url);
+        $('.profile-name').text(data.name);
+        $('.profile-username').text('@' + data.login);
+        $('.numbers-item:eq(0)').text(data.public_repos);
+        $('.numbers-item:eq(1)').text(data.followers);
+        $('.numbers-item:eq(2)').text(data.following);
+        $('.profile-link').attr('href', data.html_url);
+    },
+        error: function() {
+        alert('Não foi possível buscar os dados do usuário.');
+    }
+    });
+});
